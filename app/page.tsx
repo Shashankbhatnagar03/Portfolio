f71e49a3cc40e93a50bf670b167e00a31f9ffbfc
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-08-29 18:06:22 +0530

    fixed UI bugs

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import Contact from "./components/Contact";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden ">
-      <div className="max-w-7xl w-full">
+      <div className=" w-full">
         {/* <ModeToggle /> */}
         <FloatingNav navItems={navItems} />
         <Hero />
